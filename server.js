1aa6a120ce83d6419d3298148eb6a13798cc527f
Evan James <thevan777@gmail.com>
2021-09-28 11:55:07 -0400

    some edits

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const express = require('express')
+import express from 'express'
 const app = express()
 const PORT = 4000
 
 app.get('/', (req, res) => {
     res.send('hello')
+})
+
+app.post('/', (req, res) => {
+    res.send('posted')
 })
 
 app.listen(PORT, () => {
